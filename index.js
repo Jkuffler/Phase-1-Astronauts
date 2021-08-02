207cc3d53c1253ba0e630c3807f104b5157bb4b8
Howard Kwon <howie.ykwon70@gmail.com>
2021-08-02 18:35:44 -0400

    Initial drop-drag function

---
+++ index.js
@@ -0,0 +1,33 @@
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.container')
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging');
+    })
+
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging');
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', (e) => {
+        // const afterElement = document.querySelector(container, e.clientY)
+        e.preventDefault();
+        const currentDrag = document.querySelector('.dragging')
+        container.appendChild(currentDrag)
+    })
+})
+
+// function dragElementPlacement(container, y) {
+//     const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+//     draggableElements.reduce((closestEl, child) => {
+//         const box = child.getBoundingClientRect()
+//         console.log(box)
+//     }, {offset: Number.POSITIVE_INFINITY})
+// }
+
+
+
