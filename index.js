24f954a79e9a3e767565756f37b50b789799e4f3
Jason <jakuffler@gmail.com>
2021-08-03 10:12:52 -0400

    Uniform astro images now linked

--- index.js
+++ index.js
@@ -10,38 +10,5 @@ function getAstros(){
 })
 }
 
-//image sources ISS
 
-//PYTOR DUBROV
-//<img src="https://www.nasa.gov/sites/default/files/thumbnails/image/jsc2021e010288.jpg" alt="Pyotr Dubrov" class="astro-image" class = #astro-image>
-
-
-//AKIHIKO HOSHIDE
-//<img src ="https://cdn.mos.cms.futurecdn.net/uAUQUMbfYxP9CmkFK6oXAC-970-80.jpg.webp" alt="Akihiko Hoshide" class="astro-image">
-
-//SHANE KIMBROUGH
-//<img src ="https://upload.wikimedia.org/wikipedia/commons/0/01/Shane_Kimbrough%2C_official_portrait_%282020%29_%28cropped%29.jpg" alt = "Shane Kimbrough" class="astro-image">
-
-//MEGAN McARTHUR
-//<img src= "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/25450113025_b5c83c2c07_o.jpg" alt="Megan McArthur" class="astro-image">
-
-//OLEG NOVITSKIY
-//<img src = "https://en.wikipedia.org/wiki/Oleg_Novitsky#/media/File:Oleg_Novitskiy_2016.jpg" alt = "Oleg Novitskiy" class ="astro-image">
-
-//THOMAS PESQUET
-//<img src = "https://en.wikipedia.org/wiki/Thomas_Pesquet#/media/File:Thomas_Pesquet,_official_portrait_in_EMU_(2020).jpg" alt = "Thomas Pesquet" class = "astro-image">
-
-//MARK VANDE HEI
-//<img src = "https://en.wikipedia.org/wiki/Mark_T._Vande_Hei#/media/File:Mark_Vande_Hei,_official_portrait.jpg" alt = "Mark Vande Hei" class = "astro-image">
-
-//IMAGE SOURCES Tiangong Space Station
-
-//NEI HAISHENG
-//<img src = "https://en.wikipedia.org/wiki/Nie_Haisheng#/media/File:Nie_Haisheng_2021.png" alt = "Nie Haisheng" class = "astro-image">
-
-//LIU BOMING
-//<img src = "https://en.wikipedia.org/wiki/Liu_Boming_(astronaut)#/media/File:Liu_Boming.JPG" alt = "Liu Boming" class = "astro-image">
-
-//TANG HONGBO
-//<img src = "https://en.wikipedia.org/wiki/Tang_Hongbo#/media/File:Tang_Hongbo.jpg" alt = "Tang Hongbo" class = "astro-image">
 
